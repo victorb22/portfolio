76af0c56afd644165b335b655b9cf301d00caa16
Victor Barajas <victorbarajas@Victors-MacBook-Air-7.local>
2025-03-24 13:05:05 -0700

    upd

---
+++ script.js
@@ -0,0 +1,40 @@
+(function(){
+    'use strict';
+    console.log('READING JS');
+
+    const p0 = document.querySelector('#project0').addEventListener('click', function(){
+        window.location.href = "postpage4.html";
+    })
+
+    const p1 = document.querySelector('#project1').addEventListener('click', function(){
+        window.location.href = "postpage3.html";
+    })
+
+    const p2 = document.querySelector('#project2').addEventListener('click', function(){
+        window.location.href = "postpage9.html";
+    })
+
+    const p3 = document.querySelector('#project3').addEventListener('click', function(){
+        window.location.href = "postpage8.html";
+    })
+
+    const p4 = document.querySelector('#project4').addEventListener('click', function(){
+        window.location.href = "postpage7.html";
+    })
+
+    const p5 = document.querySelector('#project5').addEventListener('click', function(){
+        window.location.href = "postpage6.html";
+    })
+
+    const p6 = document.querySelector('#project6').addEventListener('click', function(){
+        window.location.href = "postpage5.html";
+    })
+
+    const p7 = document.querySelector('#project7').addEventListener('click', function(){
+        window.location.href = "postpage2.html";
+    })
+
+    const p8 = document.querySelector('#project8').addEventListener('click', function(){
+        window.location.href = "postpage1.html";
+    })
+}());
